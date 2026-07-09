1390acc57262ba6ea1a754a05216a2fbc813cded
Luca Tomarelli <lucatomarelli1@gmail.com>
2026-07-09 16:03:00 +0200

    check for winner

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,12 +4,6 @@
 # If you run `uv run python tic_tac_toe.py` in the command line the game will start. Try it out! ;)
 
 board = [" " for n in range(9)]
-# player_one_name = ""
-# player_two_name = ""
-# player_one_symbol = ""
-# player_two_symbol = ""
-# current_player = player_one_name
-# current_symbol = player_one_symbol
 
 def draw_board(board):
     display = []
@@ -24,6 +18,21 @@ def draw_board(board):
     print(display[3] + " | " + display[4] + " | " + display[5])
     print("---------")
     print(display[6] + " | " + display[7] + " | " + display[8])
+
+def check_winner(board):
+    winning_combos = [
+        [0,1,2], [3,4,5], [6,7,8],  # rows
+        [0,3,6], [1,4,7], [2,5,8],  # columns
+        [0,4,8], [2,4,6]            # diagonals
+    ]
+
+    for combo in winning_combos:
+        a, b, c = combo
+        if board[a] == board[b] == board[c] and board[a] != " ":
+            print(f"winner is {board[a]}")
+            return True  # returns "X" or "O" as the winner
+
+    return False  # no winner yet
 
 # Function for displaying the board
 def displayBoard(board: list):
@@ -45,41 +54,22 @@ def displayBoard(board: list):
         picked_cell = input("pick your cell (1 - 9)")
         print(f"picked cell is {picked_cell}")
         index = int(picked_cell) -1
-        board[index] = current_symbol
-        draw_board(board)
-        if current_player == player_one_name:
-            current_player = player_two_name
-            current_symbol = player_two_symbol
+        if board[index] == " ":
+            board[index] = current_symbol
+            if current_player == player_one_name:
+                current_player = player_two_name
+                current_symbol = player_two_symbol
+            else:
+                current_player = player_one_name
+                current_symbol = player_one_symbol
         else:
-            current_player = player_one_name
-            current_symbol = player_one_symbol
+            print("cell already taken, please pick again")
 
-# def assign_players():
-#     player_one_name = input("Hi, player one. What is your name?")
-#     player_two_name = input("Hi, player two. What is your name?") 
-#     print(player_one_name)
-#     print(player_two_name)
-#     player_one_symbol = input(f"Hi {player_one_name}, choose a symbol: 'X' or 'O'")
-#     player_two_symbol = input(f"Hi {player_two_name} player two, choose a symbol: 'X' or 'O'")
-#     print(f"Hi {player_one_name}, your symbol is {player_one_symbol}")
-#     print(f"Hi {player_two_name}, your symbol is {player_two_symbol}")
-
-# def pick_cell():
-#      for turn in range(9):
-#          print(f"Is {current_player}'s turn")
-#          picked_cell = input("pick your cell (1 - 9)")
-#          print(f"picked cell is {picked_cell}")
-#          index = picked_cell -1
-#          board[index] = current_symbol
-#          if current_player == player_one_name:
-#             current_player = player_two_name
-#             current_symbol = player_two_symbol
-#          else:
-#              current_player = player_one_name
-#              current_symbol = player_one_symbol
-         
+        draw_board(board)
         
-             
+        win = check_winner(board)
+        if win:
+            break
 
 
 
